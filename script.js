f5423631090fa1ecb5c44b39e640425bbf15222f
chizk0 <jose.amador.salazar@est.una.ac.cr>
2023-03-17 17:57:13 -0600

    [ADD]
     Se agrego los opciones del select desde otro archivo JS Se agreso el metodo limpiar para el input del dato Se agrego una funcion de formula

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-const formulasSelect = document.querySelector("#formulas");
+const caseSelect = document.querySelector("#select-case");
+const inputData = document.querySelector('#data');
+const finalResult = document.querySelector("#final");
+const btnClear = document.querySelector('.clear');
 
-function optionsFormula() {
-    options.forEach((optionVec) => {
-    const option = document.createElement("option");
-    option.value = optionVec.code;
-    option.innerHTML = optionVec.name;
-    formulasSelect.appendChild(option);
-    });
+const mymap = new Map();
+
+mymap.set("1",celsiusToKelvin());
+mymap.set("2",celsiusToKelvin());
+mymap.set("3",celsiusToKelvin());
+mymap.set("4",celsiusToKelvin());
+mymap.set("5",celsiusToKelvin());
+mymap.set("6",celsiusToKelvin());
+
+
+function insertOptions(){
+    options.forEach((op) => {
+        const option = document.createElement("option");
+        option.value = op.case;
+        option.innerHTML = op.case;
+        caseSelect.appendChild(option);
+    })
 }
+
+
+function clearInput(){
+    inputData.value = "";
+    console.log("fin");
+    finalResult.value = "";
+}
+
+
+
+function celsiusToKelvin(){
+    console.log( typeof(parseFloat(inputData.value) ));
+    console.log( parseFloat(inputData.value) + 273.15);
+    return parseFloat(inputData.value) + 273.15;
+}
+
+
 function main(){
-    optionsFormula();
+    insertOptions();
+    celsiusToKelvin();
+    btnClear.addEventListener("click" , clearInput);
+    console.log("fin");
 }
 main();
